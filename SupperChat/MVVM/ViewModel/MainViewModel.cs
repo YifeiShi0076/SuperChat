e6301f10937736d02137be993d7381fcc297d974
Sailing-by-Wind <1206927026@qq.com>
2025-06-04 14:36:36 +0800

    添加项目文件。

---
+++ SupperChat/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,97 @@
+﻿using SupperChat.Core;
+using SupperChat.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupperChat.MVVM.ViewModel
+{
+	class MainViewModel:ObservableObject
+	{
+		public ObservableCollection<MessageModel> Messages { get; set; }
+		public ObservableCollection<ContactModel> Contacts { get; set; }
+
+		/* Command */
+		public RelayCommand SendCommand { get; set; }
+		private ContactModel _selectedContact;
+		public ContactModel SelectedContact {
+			get { return _selectedContact; }
+			set { _selectedContact = value; 
+				OnPropertyChanged();
+			}
+		}
+		private string _message;
+		public string Message { 
+			get { return _message; }
+			set { _message = value; 
+				OnPropertyChanged(); 
+			}
+		}
+		public MainViewModel()
+		{
+			Messages = new ObservableCollection<MessageModel>();
+			Contacts = new ObservableCollection<ContactModel>();
+
+			SendCommand = new RelayCommand(o =>
+			{
+				if (string.IsNullOrEmpty(Message)) return;
+				Messages.Add(new MessageModel
+				{
+					Message = Message,
+					FirstMessage = false
+				});
+				Message = string.Empty;
+			});
+
+			Messages.Add(new MessageModel
+			{
+				Username = "John Doe",
+				UsernameColor = "#409aff",
+				ImageSource = "../Icons/1.jpg",
+				Message = "Hello, this is a test message from John!",
+				Time = DateTime.Now,
+				IsNativeOrigin = false,
+				FirstMessage = true
+			});
+
+			for (int i = 0; i < 1; i++)
+			{
+				Messages.Add(new MessageModel
+				{
+					Username = "Bunny",
+					UsernameColor = "#409aff",
+					ImageSource = "../Icons/0.jpg",
+					Message = "Hello, this is a test message from Bunny!",
+					Time = DateTime.Now,
+					IsNativeOrigin = true
+				});
+			}
+
+			for (int i = 0; i < 1; i++)
+			{
+				Messages.Add(new MessageModel
+				{
+					Username = "Bunny",
+					UsernameColor = "#409aff",
+					ImageSource = "../Icons/0.jpg",
+					Message = "Last Message.",
+					Time = DateTime.Now,
+					IsNativeOrigin = true
+				});
+			}
+
+			for (int i = 0; i < 3; i++)
+			{
+				Contacts.Add(new ContactModel
+				{
+					Username = $"Allison{i}",
+					ImageSource = "../Icons/1.jpg",
+					Messages = Messages
+				});
+			}
+		}
+	}
+}
---
+++ SupperChat/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SupperChat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+			InitializeComponent();
+        }
+
+		private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+            if(e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+
+		}
+
+		private void ButtonMinimize_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.MainWindow.WindowState = WindowState.Minimized;
+		}
+
+		private void ButtonMaximize_Click(object sender, RoutedEventArgs e)
+		{
+			if(Application.Current.MainWindow.WindowState == WindowState.Maximized)
+			{
+				Application.Current.MainWindow.WindowState = WindowState.Normal;
+			}
+			else
+			{
+				Application.Current.MainWindow.WindowState = WindowState.Maximized;
+			}
+		}
+
+		private void CloseButton_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+
+		private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+
+        }
+    }
+}
