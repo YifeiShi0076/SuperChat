5e706d8d9097541b73623958185a6a5f6ab8450f
42211096lgh <128457626@qq.com>
2025-06-09 12:11:10 +0800

    1

--- SupperChat/MVVM/View/Window1.xaml.cs
+++ SupperChat/MVVM/View/Window1.xaml.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System.Collections.Specialized;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SupperChat.MVVM.Model;
+using SupperChat.MVVM.ViewModel;
+using SupperChat.Views;
